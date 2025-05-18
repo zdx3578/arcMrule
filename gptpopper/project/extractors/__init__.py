025ca6c94544cc9cb6dae282ad528cef904e2648
zdx <179363811@qq.com>
2025-05-18 18:01:55 +0800

    popper gpt

--- gptpopper/project/extractors/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pixel import PixelExtractor
-from .bars import BarsExtractor
-EXTRACTOR_MAP = {
-    "pixel": PixelExtractor,
-    "bars":  BarsExtractor,
-}
--- gptpopper/project/util/grid_io.py
+++
@@ -1,6 +0,0 @@
-import json, numpy as np
-COLMAP = {0:'bg',1:'cyan',2:'red',3:'blue',4:'yellow',5:'green'}
-
-def load_pair(path):
-    d = json.load(open(path))
-    return np.array(d['train'][0]['input']), np.array(d['train'][0]['output'])
